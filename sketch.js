c78c9be25e1d6cab5ec30b3a7ee827aae92ccda8
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-02-21 17:25:02 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
 function draw(){
   background("white");
-  
+  if (position !== undefined){
     if(keyDown(LEFT_ARROW)){
       writePosition(-1,0);
     }
@@ -31,11 +31,14 @@ function draw(){
       writePosition(0,+1);
     }
     drawSprites();
-  
+  }
 }
 
 function writePosition(x,y){
-  
+  database.ref('ball/position').set({
+    'x': position.x + x ,
+    'y': position.y + y
+  })
 }
 
 function readPosition(data){
