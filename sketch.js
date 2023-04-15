44ee70ec1bedea5f946aacd17e663014b0b56ecd
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-04-15 15:24:48 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@ var hypnoticBall, database;
 var position;
 function setup(){
   database = firebase.database();
-  console.log(database);
   createCanvas(500,500);
   hypnoticBall = createSprite(250,250,10,10);
   hypnoticBall.shapeColor = "red";
